0db0741c3bb5c36b9c5f746d4bdf8d3965758743
Shubhram Krishna <shubhram.krishna@tavant.com>
2020-04-30 00:04:46 +0530

    code cleanup

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/users', require('./users'));
-router.use('/domains', require('./domains'));
-router.use('/roles', require('./roles'));
-router.use('/domainslog', require('./domainsLog'));
-
-module.exports = router;
